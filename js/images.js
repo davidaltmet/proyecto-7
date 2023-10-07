ed39a26fcba9cc5f48cb1d49b2e422e523ac2311
david restrepo <dr20giraldo@gmail.com>
2023-10-07 16:00:11 -0500

    modificando carrusel de imagenes

--- js/images.js
+++ js/images.js
@@ -8,4 +8,21 @@ $(document).ready(function () {
     prevArrow: $(".flecha1"), // Selector para la flecha izquierda
     nextArrow: $(".flecha2"), // Selector para la flecha derecha
   });
+
+  $("#efecto_tenis_images").slick({
+    slidesToShow: 4, // Muestra 4 miniaturas a la vez
+    slidesToScroll: 1,
+    asNavFor: "#img_efecto_tenis", // ID del slider principal de imágenes
+    dots: false,
+    focusOnSelect: true,
+  });
+  $("#img_efecto_tenis").on(
+    "afterChange",
+    function (event, slick, currentSlide) {
+      // Remover la clase "active" de todas las miniaturas
+      $(".ef_img").removeClass("active");
+      // Agregar la clase "active" a la miniatura correspondiente
+      $(".ef_img").eq(currentSlide).addClass("active");
+    }
+  );
 });
